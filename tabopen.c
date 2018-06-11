7bf3574f0c99390d045b9b652815126fbf4b7873
DAMNSOCIETY <40157882+DAMNSOCIETY@users.noreply.github.com>
2018-06-11 00:27:53 -0400

    Add files via upload

---
+++ tabopen.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	while(1)
+	{
+	  system("firefox fb.com --new-tab");
+	}
+	return 0;
+}
---
+++ vs.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+  system("firefox https://doc-00-bg-docs.googleusercontent.com/docs/securesc/tvg9c93k04uh52negiqtkmmt6iftpu7q/329te5m31trdtgkrki6rpumn1tfun571/1528646400000/05028026763474943192/05028026763474943192/1qK6K0ANH_xK_aTH3aySNOluXi3oitqlQ?e=download&nonce=es1420i6csvpc&user=05028026763474943192&hash=m8qs5dbjtgmm03688e1m12lh0su50f5m");
+  return 0;
+}
